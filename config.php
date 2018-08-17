37096499b83a38b5ecc84421396ae76991e4f186
Kellan <gellinn@uw.edu>
2018-08-17 14:38:14 -0700

    Added stats plugin, made navbar link to stats, and wiki plugin for wiki page creation within the app.  The wiki page creation doesn't seem to work. Will figure this bug out later

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'db.host' => 'localhost',
+  'db.port' => '3306',
+  'db.user' => 'buildingintellect_site_admin',
+  'db.pass' => '890p890p',
+  'db.name' => 'phproject',
+);
