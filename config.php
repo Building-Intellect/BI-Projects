21900a955e82945c4d47c2dc2c25466726017dcb
kellan <gellinn@uw.edu>
2018-09-05 12:04:37 -0700

    Remove config.php from project and ignore it

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array (
-  'db.host' => 'localhost',
-  'db.port' => '3306',
-  'db.user' => 'buildingintellect_site_admin',
-  'db.pass' => '890p890p',
-  'db.name' => 'buildingintellect-projects',
-);
