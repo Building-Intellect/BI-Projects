d79d5dd389ec27cfd1be61b0a9b733345a90ac41
Kellan <gellinn@uw.edu>
2018-12-06 17:55:56 -0800

    Added single page fetch for pdf viewer:
    -need to remove uploading and scale to 4 pages at a time
    -need to add zoom and text layer
    -need to populate viewer options from text parsed from pdf

--- js/behavior.js
+++ js/behavior.js
@@ -53,4 +53,117 @@
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
         return check;
     };
+
+	// functions and variables for project plans pdf viewer
+	var __PDF_DOC,
+		__CURRENT_PAGE,
+		__TOTAL_PAGES,
+		__PAGE_RENDERING_IN_PROGRESS,
+		__CANVAS,
+		__CANVAS_CTX;
+
+	window.initializePdfViewer = function () {
+		__PAGE_RENDERING_IN_PROGRESS = 0;
+		__CANVAS = $('#pdf-canvas').get(0);
+		__CANVAS_CTX = __CANVAS.getContext('2d');
+
+		// Upon click this should should trigger click on the #file-to-upload file input element
+		// This is better than showing the not-good-looking file input element
+		$("#upload-button").on('click', function() {
+			$("#file-to-upload").trigger('click');
+		});
+
+		// When user chooses a PDF file
+		$("#file-to-upload").on('change', function() {
+			// Validate whether PDF
+		    if(['application/pdf'].indexOf($("#file-to-upload").get(0).files[0].type) == -1) {
+		        alert('Error : Not a PDF');
+		        return;
+		    }
+
+			$("#upload-button").hide();
+
+			// Send the object url of the pdf
+			showPDF(URL.createObjectURL($("#file-to-upload").get(0).files[0]));
+		});
+
+		// Previous page of the PDF
+		$("#pdf-prev").on('click', function() {
+			if(__CURRENT_PAGE != 1)
+				showPage(--__CURRENT_PAGE);
+		});
+
+		// Next page of the PDF
+		$("#pdf-next").on('click', function() {
+			if(__CURRENT_PAGE != __TOTAL_PAGES)
+				showPage(++__CURRENT_PAGE);
+		});
+	}
+
+	function showPDF(pdf_url) {
+		$("#pdf-loader").show();
+
+		PDFJS.getDocument({ url: pdf_url }).then(function(pdf_doc) {
+			__PDF_DOC = pdf_doc;
+			__TOTAL_PAGES = __PDF_DOC.numPages;
+
+			// Hide the pdf loader and show pdf container in HTML
+			$("#pdf-loader").hide();
+			$("#pdf-contents").show();
+			$("#pdf-total-pages").text(__TOTAL_PAGES);
+
+			// Show the first page
+			showPage(1);
+		}).catch(function(error) {
+			// If error re-show the upload button
+			$("#pdf-loader").hide();
+			$("#upload-button").show();
+
+			alert(error.message);
+		});;
+	}
+
+	function showPage(page_no) {
+		__PAGE_RENDERING_IN_PROGRESS = 1;
+		__CURRENT_PAGE = page_no;
+
+		// Disable Prev & Next buttons while page is being loaded
+		$("#pdf-next, #pdf-prev").attr('disabled', 'disabled');
+
+		// While page is being rendered hide the canvas and show a loading message
+		$("#pdf-canvas").hide();
+		$("#page-loader").show();
+
+		// Update current page in HTML
+		$("#pdf-current-page").text(page_no);
+
+		// Fetch the page
+		__PDF_DOC.getPage(page_no).then(function(page) {
+			// As the canvas is of a fixed width we need to set the scale of the viewport accordingly
+			var scale_required = __CANVAS.width / page.getViewport(1).width;
+
+			// Get viewport of the page at required scale
+			var viewport = page.getViewport(scale_required);
+
+			// Set canvas height
+			__CANVAS.height = viewport.height;
+
+			var renderContext = {
+				canvasContext: __CANVAS_CTX,
+				viewport: viewport
+			};
+
+			// Render the page contents in the canvas
+			page.render(renderContext).then(function() {
+				__PAGE_RENDERING_IN_PROGRESS = 0;
+
+				// Re-enable Prev & Next buttons
+				$("#pdf-next, #pdf-prev").removeAttr('disabled');
+
+				// Show the canvas and hide the page loader
+				$("#pdf-canvas").show();
+				$("#page-loader").hide();
+			});
+		});
+	}
 })();
